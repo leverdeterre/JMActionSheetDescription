621102f5babdaa941bcc0a6e9a5d501fba150f3f
Jerome Morissard <jerome.morissard@backelite.com>
2015-05-25 00:45:14 +0200

    add missing files

---
+++ Example/Pods/Target Support Files/Pods-JMActionSheetDescription_Example-ionicons/Pods-JMActionSheetDescription_Example-ionicons-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "ionicons-codes.h"
+#import "IonIcons.h"
+
+FOUNDATION_EXPORT double ioniconsVersionNumber;
+FOUNDATION_EXPORT const unsigned char ioniconsVersionString[];
+
