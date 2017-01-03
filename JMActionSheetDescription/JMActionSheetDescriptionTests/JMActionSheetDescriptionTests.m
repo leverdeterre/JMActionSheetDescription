5ca98da65dadde39b9ad7f691928fd38995c565e
Jérôme Morissard <morissardj@gmail.com>
2017-01-03 11:22:19 +0100

    Adding Carthage framework project

---
+++ JMActionSheetDescription/JMActionSheetDescriptionTests/JMActionSheetDescriptionTests.m
@@ -0,0 +1,39 @@
+//
+//  JMActionSheetDescriptionTests.m
+//  JMActionSheetDescriptionTests
+//
+//  Created by jerome morissard on 03/01/2017.
+//  Copyright © 2017 jerome morissard. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JMActionSheetDescriptionTests : XCTestCase
+
+@end
+
+@implementation JMActionSheetDescriptionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
